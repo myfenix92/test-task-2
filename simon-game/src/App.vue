311a225e85ce2652f4f4721b22677133cb22dcdc
Yana Kornejkova <bloom.hog@gmail.com>
2021-04-08 00:12:15 +0300

    feat: add components for button and circle

--- simon-game/src/App.vue
+++ simon-game/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-    <MainBlock countRound="0"/>
+    <MainBlock/>
+    <CircleBlock/>
+    <ButtonBlock/>
   </div>
 </template>
 
 <script>
-import MainBlock from './components/HelloWorld.vue'
+import MainBlock from './components/MainBlock.vue'
+import ButtonBlock from './components/Button.vue'
+import CircleBlock from './components/Circle.vue'
 
 export default {
   name: 'App',
   components: {
-    MainBlock
+    MainBlock,
+    CircleBlock,
+    ButtonBlock
   }
 }
 </script>
