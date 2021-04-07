e99c76986a7af948b04223f6af8ba4ee9845691f
Yana Kornejkova <bloom.hog@gmail.com>
2021-04-08 00:15:23 +0300

    feat: add comments, change script

--- simon-game/src/components/MainBlock.vue
+++ simon-game/src/components/MainBlock.vue
@@ -2,24 +2,23 @@
   <div class="main-block">
     <h1>Simon Game</h1>
     <div class="game-block">
-      <div class="circle-block">
+      <!-- <div class="circle-block">
         <ul>
           <li class="red circle" data-title="1"></li>
           <li class="green circle" data-title="2"></li>
           <li class="yellow circle" data-title="3"></li>
           <li class="blue circle" data-title="4"></li>
         </ul>
-      </div>
+      </div> -->
       <div class="options-block">
-        <p>Раунд: <span> {{ countRound }} </span></p>
-        <p class="result-game"> {{ msgGame }} </p>
+        <p>Раунд: <span> {{$root.level}} </span></p>
+        <!-- <p class="result-game"> {{ msgGame }} </p> -->
         <span>Уровень:</span>
         <div class="level-blocks">
-            <label for="easy-level"> <input type="radio" id="easy-level" name="levelChoose" value="easyData"> Легкий</label>
+            <label for="easy-level"> <input type="radio" id="easy-level" name="levelChoose" value="easyData" checked> Легкий</label>
             <label for="middle-level"> <input type="radio" id="middle-level" name="levelChoose" value="middleData"> Средний</label>
             <label for="hard-level"> <input type="radio" id="hard-level" name="levelChoose" value="hardData"> Сложный</label>
         </div>
-        <button>Старт</button>
       </div>
     </div>
   </div>
@@ -28,10 +27,11 @@
 <script>
 export default {
   name: 'MainBlock',
-  props: {
-    msgGame: String,
-    countRound: Number,
-  }
+  // data() {
+  //   return {
+  //     countRound: 0,
+  //   }
+  // },
 }
 </script>
 
