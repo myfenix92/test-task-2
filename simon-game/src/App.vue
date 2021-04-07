95edf72881da81cab5cd1b1d745ba379c4424596
Yana Kornejkova <bloom.hog@gmail.com>
2021-04-07 16:56:47 +0300

    init

---
+++ simon-game/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <MainBlock countRound="0"/>
+  </div>
+</template>
+
+<script>
+import MainBlock from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainBlock
+  }
+}
+</script>
---
+++ simon-game/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main-block">
+    <h1>Simon Game</h1>
+    <div class="game-block">
+      <div class="circle-block">
+        <ul>
+          <li class="red circle" data-title="1"></li>
+          <li class="green circle" data-title="2"></li>
+          <li class="yellow circle" data-title="3"></li>
+          <li class="blue circle" data-title="4"></li>
+        </ul>
+      </div>
+      <div class="options-block">
+        <p>Раунд: <span> {{ countRound }} </span></p>
+        <p class="result-game"> {{ msgGame }} </p>
+        <span>Уровень:</span>
+        <div class="level-blocks">
+            <label for="easy-level"> <input type="radio" id="easy-level" name="levelChoose" value="easyData"> Легкий</label>
+            <label for="middle-level"> <input type="radio" id="middle-level" name="levelChoose" value="middleData"> Средний</label>
+            <label for="hard-level"> <input type="radio" id="hard-level" name="levelChoose" value="hardData"> Сложный</label>
+        </div>
+        <button>Старт</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainBlock',
+  props: {
+    msgGame: String,
+    countRound: Number,
+  }
+}
+</script>
+
