4c370bd74dc87dcf39043bc7201abc6f7ff7e645
Yana Kornejkova <bloom.hog@gmail.com>
2021-04-08 20:51:37 +0300

    feat: move tags, change endGame method

--- simon-game/src/components/Level.vue
+++ simon-game/src/components/Level.vue
@@ -1,13 +1,13 @@
 <template>
       <div class="options-block">
-        <p>Раунд: <span> {{$root.level}} </span></p>
-        <p class="result-game" v-if="seen"> {{ msgGame }} </p>
+        <p>Раунд:<span>{{$root.arraySoundNum.length}}</span></p>
         <span>Уровень:</span>
         <div class="level-blocks">
             <label for="easy-level"> <input type="radio" id="easy-level" name="levelChoose" value="1500" checked> Легкий</label>
             <label for="middle-level"> <input type="radio" id="middle-level" name="levelChoose" value="1000"> Средний</label>
             <label for="hard-level"> <input type="radio" id="hard-level" name="levelChoose" value="400"> Сложный</label>
         </div>
+        <p v-show="seen"> {{ msgGame }}</p>
       </div>
 </template>
 
@@ -24,18 +24,15 @@ export default {
   },
 
   methods: {
-
-    endGame() {
-      if (this.$root.arraySoundNum.length === 0) {
-        this.seen = true;
-        this.msgGame = `Вы проиграли после ${this.$root.level} раундов!`
-      }
+    endGame(bool) {
+        this.seen = bool;
+        this.msgGame = `Вы проиграли после ${this.$root.arraySoundNum.length} раундов!`
     }
   },
 
   mounted() {
-    eventBus.$on('msgGame', () => {
-      this.endGame();
+    eventBus.$on('msgGame', (bool) => {
+      this.endGame(bool);
     })
 }
 }
