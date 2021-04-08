cbfc3ee41a2a3bd421fc9db8fa83f3672f7530ae
Yana Kornejkova <bloom.hog@gmail.com>
2021-04-08 23:07:39 +0300

    feat: add cursor behavior

--- simon-game/src/components/Button.vue
+++ simon-game/src/components/Button.vue
@@ -25,6 +25,11 @@ export default {
       const levelTime = Number(Array.from(document.querySelectorAll('input')).find((el) => el.checked).value);
       const circle = document.querySelectorAll('.circle');
       document.querySelector('.circle-block').classList.add('reset-game-event');
+      document.querySelector('.circle-block').classList.remove('cursor-view');
+      setTimeout(() => {
+        document.querySelector('.circle-block').classList.remove('reset-game-event');
+        document.querySelector('.circle-block').classList.add('cursor-view');
+      }, ((this.$root.arraySoundNum.length - 1) * levelTime) + this.$root.timeHighlight);
       for (let i = 0; i < this.$root.arraySoundNum.length; i += 1) {
         // eslint-disable-next-line import/no-dynamic-require
         const audio = new Audio(require(`../assets/sounds/${this.$root.arraySoundNum[i]}.mp3`)); // eslint-disable-line global-require
