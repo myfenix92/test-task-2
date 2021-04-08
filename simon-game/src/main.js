55599f6856b415eecc4dfef3855434bf465c7900
Yana Kornejkova <bloom.hog@gmail.com>
2021-04-08 23:06:33 +0300

    fix: add eslint rule

--- simon-game/src/main.js
+++ simon-game/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import Store from './components/Store';
 import './styles/style.css';
 
-export const eventBus = new Vue();
+export const eventBus = new Vue(); // eslint-disable-line import/prefer-default-export
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
